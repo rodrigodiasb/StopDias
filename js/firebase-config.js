0397540d5f44239d3684dd18d3e8c86ecc9e5fe9
rodrigodiasb <62857786+rodrigodiasb@users.noreply.github.com>
2025-11-03 11:58:18 -0400

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,16 @@
+// js/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  databaseURL: "https://SEU_PROJETO-default-rtdb.firebaseio.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "SEU_ID",
+  appId: "SEU_APP_ID"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
